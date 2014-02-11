2006ba169570417abd271bfd47d3737de2a801f0
Andrew Hill <andrewjrhill@gmail.com>
2014-02-11 01:04:57 -0500

    Tooltip added to community button. New Markup.

--- js/balloons.js
+++ js/balloons.js
@@ -1,11 +1,26 @@
-$('.last-nav').hover(function(){
-    $(this).toggleClass('last-nav-hover');
-});
-
 $('.get-started-button').hover(function(){
     $(this).toggleClass('get-started-button-hover');
 });
 
-// $('#nav').children().eq(0).children().children().hover(function(){
-//     $('.active-arrows').toggleClass('nav-hover');
-// });
+// Tooltip
+var tooltipInfo = 'We have selected a small handful of charities which are dedicated to science education and the progression around the world. <strong>We hope you will consider giving to one of these charities by clicking on Help The Community. </strong>100% of your donations are given to the charity of your choice. ';
+var tooltipStyles = {
+  top: $('.last-nav').offset().top + 35 + 'px',
+  left: Math.round($('.last-nav').offset().left) + 'px'
+};
+
+var tooltip = document.createElement('div');
+    tooltip.innerHTML = tooltipInfo;
+    tooltip.className = 'help-tooltip';
+    tooltip.style.position = 'absolute';
+    tooltip.style.top = tooltipStyles.top;
+    tooltip.style.left = tooltipStyles.left;
+
+$('.last-nav').mouseover(function(){
+  $(this).toggleClass('last-nav-hover');
+  $(this).parent().after(tooltip);
+});
+$('.last-nav').mouseout(function(){
+  $(this).toggleClass('last-nav-hover');
+  $('.help-tooltip').remove();
+});

b1d3e233cdd66bd7a545572b7232feb0cfc81b9b
Andrew Hill <andrewjrhill@gmail.com>
2014-02-10 23:03:07 -0500

    Initial Commit

---
+++ js/balloons.js
@@ -0,0 +1,11 @@
+$('.last-nav').hover(function(){
+    $(this).toggleClass('last-nav-hover');
+});
+
+$('.get-started-button').hover(function(){
+    $(this).toggleClass('get-started-button-hover');
+});
+
+// $('#nav').children().eq(0).children().children().hover(function(){
+//     $('.active-arrows').toggleClass('nav-hover');
+// });

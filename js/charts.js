1c0b748a5214d1145b945fa5b4f6a82e73516dcc
Cole Gentry <cole.gentry@ally.com>
2014-04-15 22:54:02 -0400

    added all raw launch data to /data folder; linked all data on launches page allowing users to download the raw data for personal consumption; added the second launch charts; added custom styles for launches page; fixed small typo on index page;

--- js/charts.js
+++ js/charts.js
@@ -1,3 +1,102 @@
+//<----------------------Launch 2 Below--------------------------------->>>>
+//Line chart below is for the 2nd launches altitude data-------------------------------------
+$(function () {
+    $('#altitude1').highcharts({
+        title: {
+            text: 'Weather Balloon Mk2 Flight',
+            x: -20 //center
+        },
+        credits: {
+            enabled: false
+        },
+        subtitle: {
+            text: 'Altitude Summary',
+            x: -20
+        },
+        xAxis: {
+            title: {
+                text: 'Time (Minutes)'
+            },
+            categories: ['0', '15', '30', '45', '60', '75', '90',
+                '105', '120', '135', '150', '165', '180']
+        },
+        yAxis: {
+            title: {
+                text: 'Altitude(ft)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            valueSuffix: 'ft'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'center',
+            verticalAlign: 'bottom',
+            borderWidth: 0
+        },
+        series: [{
+            name: 'Height',
+            data: [164, 13502, 28792, 45541, 73477, 52387, 22553, 4073, -325]
+        }]
+    });
+});
+//Line chart below is for the 2nd launches temp data-------------------------------------
+$(function () {
+    $('#temp1').highcharts({
+        title: {
+            text: 'Weather Balloon Mk2 Tempurture Readings',
+            x: -20 //center
+        },
+        credits: {
+            enabled: false
+        },
+        subtitle: {
+            text: 'Internal and External Tempurature Summary',
+            x: -20
+        },
+        xAxis: {
+            title: {
+                text: 'Time (Minutes)'
+            },
+            categories: ['0', '15', '30', '45', '60', '75', '90',
+                '105', '120', '135', '150', '165', '180']
+        },
+        yAxis: {
+            title: {
+                text: 'Tempurature °F'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            valueSuffix: '°F'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'center',
+            verticalAlign: 'bottom',
+            borderWidth: 0
+        },
+        series: [{
+            name: 'Int. Temp',
+            data: [58, 61, 54, 37.4, 23, 19, 11.8, 26, 43]
+        },{
+            name: 'Ext. Temp',
+            data: [48, 38, -1.3, -30.8, -24, -48, -5.9, 45, 61]
+        }]
+    });
+});
+
+
+//<----------------------Launch 3 Below--------------------------------->>>>
 
 //Line chart below is for the 3rd launches altitude data-------------------------------------
 $(function () {
